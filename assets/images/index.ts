d1cf8d61514a1687c460597a99f6993d699b347c
Yaroslav131 <berserk12186@gmail.com>
2023-08-14 12:41:50 +0300

    add assets

---
+++ assets/images/index.ts
@@ -0,0 +1,5 @@
+export const welcomImg=require("./welcomImg.png")
+export const leftEllipse=require("./leftEllipse.png")
+export const rightEllipse=require("./rightEllipse.png")
+export const largeRightEllipse=require("./largeEllipse.png")
+export const searchIcon=require("./searchIcon.png")
