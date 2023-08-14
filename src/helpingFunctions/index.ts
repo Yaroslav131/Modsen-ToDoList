480b0610a3ca97cdfa0c002962169252d3195c74
Yaroslav131 <berserk12186@gmail.com>
2023-08-14 12:46:44 +0300

    add format date function

---
+++ src/helpingFunctions/index.ts
@@ -0,0 +1,10 @@
+export function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  };
+
+  return date.toLocaleDateString('en-US', options);
+}
