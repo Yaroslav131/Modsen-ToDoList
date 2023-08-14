27928d5514e3d26dcf6e7855ae0cb6c7b5fee78d
Yaroslav131 <berserk12186@gmail.com>
2023-08-14 12:41:21 +0300

    add npm files

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,9 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = {
+  resolver: {
+    extraNodeModules: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
